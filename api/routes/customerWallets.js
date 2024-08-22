bf10318e617f1edda7b48ce793580cd6be24d54e
GuilhermeOli97 <guilherme_190597@hotmail.com>
2024-08-22 16:45:09 -0300

    feat: post method

--- api/routes/customerWallets.js
+++ api/routes/customerWallets.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
   const controller = app.controllers.customerWallets;
 
-  app.route("/api/v1/customer-wallets").get(controller.listCustomerWallets);
+  app
+    .route("/api/v1/customer-wallets")
+    .get(controller.listCustomerWallets)
+    .post(controller.saveCustomerWallets);
 };
